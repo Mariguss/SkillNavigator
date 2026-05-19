9616e9bc9384ba77ac04178533968cd400ab0ff8
Mariguss <ms3079316@gmail.com>
2026-05-19 12:55:45 +0300

    dev: change user model

--- app/models/user.py
+++ app/models/user.py
@@ -8,9 +8,9 @@ from user_skills import UserSkills
 class User(BaseModelCreatedUpdated):
     __tablename__ = "user"
 
-    login: Mapped[str] = mapped_column(unique=True)
-    password_hash: Mapped[str]
-    email: Mapped[str]
+    login: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
 
     applications: Mapped[List["Application"]] = relationship(back_populates="user")
     user_skills: Mapped[List["UserSkills"]] = relationship(back_populates="user")
