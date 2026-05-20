8a522b660a2a410e8e0fb11afef8661b7cbfc45d
Mariguss <ms3079316@gmail.com>
2026-05-20 12:49:04 +0300

    dev: add new files, add req

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter, Depends
 
 from app.core.container import Container
 from app.core.dependencies import get_current_active_user
-from app.schema.auth_schema import SignIn, SignInResponse, SignUp
-from app.schema.user_schema import User
+from app.schemas.auth_schema import SignIn, SignInResponse, SignUp
+from app.schemas.user_schema import User
 from app.services.auth_service import AuthService
 
 router = APIRouter(
--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,29 +1,30 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
-from app.schema.user_schema import User
+from app.schemas.user_schema import User
 
 
 class SignIn(BaseModel):
-    email__eq: str
+    login: str
     password: str
 
 
 class SignUp(BaseModel):
-    email: str
-    password: str
-    name: str
+    email: EmailStr  # Автоматическая валидация корректности почты
+    password: str = Field(..., min_length=6)
+    login: str = Field(..., min_length=3, max_length=50)
 
 
 class Payload(BaseModel):
     id: int
     email: str
-    name: str
+    login: str
     is_superuser: bool
 
 
 class SignInResponse(BaseModel):
     access_token: str
+    token_type: str = "bearer"  # Стандарт для OAuth2/JWT в FastAPI
     expiration: datetime
     user_info: User
